97221522e3c87d377b618033f71ed9ccad8f90bd
Anouar ADLANI <anouar@adlani.com>
2011-01-11 09:21:01 +0100

    Yardoc for the HtmlTidy filter

--- lib/nanoc/toolbox/filters/html_tidy.rb
+++ lib/nanoc/toolbox/filters/html_tidy.rb
@@ -1,7 +1,18 @@
 module Nanoc::Toolbox::Filters
+  # NANOC Filter for html output tidy
+  #
+  # @author Anouar ADLANI <anouar@adlani.com>
   class HtmlTidy < Nanoc3::Filter
+    
     identifier :html_tidy
     
+    # Tidy the HTML output
+    # Runs the content through Nokogiry document parser, and request the html output, 
+    # which is tidied by default.
+    #
+    # @param [String] content The content to filter
+    # @param [String] params This method takes no options.
+    # @return [String] The filtered content
     def run(content, params = {})
       require 'nokogiri'
       Nokogiri::HTML::Document.parse(content).to_html
