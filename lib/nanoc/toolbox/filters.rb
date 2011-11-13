505214ec09f9a27416bab0f7f14900acd644ca4b
Anouar ADLANI <aadlani@eurodns.com>
2011-11-13 17:11:13 +0100

    Add the js_minify filter

--- lib/nanoc/toolbox/filters.rb
+++ lib/nanoc/toolbox/filters.rb
@@ -1,7 +1,7 @@
 require 'nanoc/toolbox/filters/html_tidy'
 require 'nanoc/toolbox/filters/add_sections'
+require 'nanoc/toolbox/filters/js_minify'
 
 # This module will regroup all the filters for nanoc
 module Nanoc::Toolbox::Filters
-
 end
