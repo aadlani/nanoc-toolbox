0de8e63b26fee8e4edcc692e318a8ba788ba9c4b
Anouar ADLANI <anouar@adlani.com>
2012-11-04 17:03:25 +0100

    Bump the version number

--- lib/nanoc/toolbox/version.rb
+++ lib/nanoc/toolbox/version.rb
@@ -4,7 +4,7 @@ module Nanoc
     module Version
       MAJOR = 0
       MINOR = 1
-      PATCH = 0
+      PATCH = 1
       BUILD = nil
 
       STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join(".")
